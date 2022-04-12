9a5d94bd3248e0469648c11ba59ac41a4e3bf829
owen <owen013288@gmail.com>
2022-04-12 20:45:16 +0800

    Razor語法與if..else條件判斷(大概展現一下就好)

---
+++ CoreExercise/Controllers/ThirdController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreExercise.Controllers
+{
+    public class ThirdController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
