49d91a63777e8ae106796cfba70dbfa8eea996d1
ERI\owen013288 <owen013288@eri.com.tw>
2022-04-13 10:35:15 +0800

    JSON在.NET Core3.1與JS 編碼、解碼學習

--- CoreExercise/Controllers/ThirdController.cs
+++ CoreExercise/Controllers/ThirdController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreExercise.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace CoreExercise.Controllers
 {
     public class ThirdController : Controller
     {
+        // using System.Collections.Generic; => List
+        // using CoreExercise.Models; => Person
+        private List<Person> persons { get; }
+        public ThirdController()
+        {
+            persons = new List<Person> {
+                new Person { Id=1, Name="Kevin", Email="kevin@gmail.com" },
+                new Person { Id=2, Name="David", Email="kevin@gmail.com" },
+                new Person { Id=3, Name="Rose", Email="kevin@gmail.com" }
+            };
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult JsonIndex()
+        {
+            return View(persons);
         }
     }
 }
