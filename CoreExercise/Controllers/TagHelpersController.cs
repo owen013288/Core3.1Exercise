a33bae7e3a52b3836393a8e10887622f69c50297
owen <owen013288@gmail.com>
2022-04-13 21:36:57 +0800

    給PartialView Model或ViewData

---
+++ CoreExercise/Controllers/TagHelpersController.cs
@@ -0,0 +1,28 @@
+﻿using CoreExercise.IService;
+using CoreExercise.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreExercise.Controllers
+{
+    public class TagHelpersController : Controller
+    {
+        //C# 6.0 - Auto Property Initializer
+        public List<Hero> heros { get; } = new List<Hero>
+        {
+            new Hero { Name = "Elon Musk", Brief="特斯拉創辦人 伊隆·馬斯克", Photo="ElonMusk.jpg", WikiUrl="https://goo.gl/46xeXx" },
+            new Hero { Name = "Mark Zuckerberg", Brief="Facebook創辦人 馬克·祖伯克", Photo="MarkZuckerberg.jpg", WikiUrl="https://goo.gl/BktGGA" },
+            new Hero { Name = "Steve Jobs", Brief="蘋果創辦人 史提夫·賈伯斯", Photo="SteveJobs.jpg", WikiUrl="https://goo.gl/nAiX0y" },
+            new Hero { Name = "Vader", Brief="帝國元帥  維達", Photo="Vader.jpg", WikiUrl="https://en.wikipedia.org/wiki/Darth_Vader" },
+            new Hero { Name = "Darth Mual", Brief="星際大戰 達斯摩", Photo="DarthMual.jpg", WikiUrl="https://goo.gl/5obLhX"},
+            new Hero { Name = "White Twilek", Brief="星際大戰 女絕地武士Twilek", Photo="WhiteTwilek.jpg", WikiUrl="https://goo.gl/reKzAu" },
+            new Hero { Name = "Obiwan", Brief="星際大戰 歐比旺Obiwan", Photo="Obiwan.jpg", WikiUrl="http://bit.ly/33gxdgt" },
+            new Hero { Name = "Merkel", Brief="德國總理 梅克爾", Photo="Merkel.jpg", WikiUrl="http://bit.ly/33huSlv" }
+        };
+
+        public IActionResult PartialTagHelper()
+        {
+            return View(heros);
+        }
+    }
+}
