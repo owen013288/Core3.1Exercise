7c20c2c88571dda7d8b92a9169d33b1aff9d8283
ERI\owen013288 <owen013288@eri.com.tw>
2022-04-13 17:29:06 +0800

    使用2.9.3版本的Chart.js搭配JSON

--- CoreExercise/Controllers/ThirdController.cs
+++ CoreExercise/Controllers/ThirdController.cs
@@ -27,5 +27,40 @@ namespace CoreExercise.Controllers
         {
             return View(persons);
         }
+
+        public IActionResult JsonChart()
+        {
+            string[] Labels = { "1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月" };
+
+            // 1.序列化成為JSON物件結構字串
+            string jsonLabels = System.Text.Json.JsonSerializer.Serialize(Labels);
+
+            // 以ViewData將資料傳給View
+            ViewData["JsonLabels"] = jsonLabels;
+
+            // 2.List集合包含台北,台中及高雄三個地方的氣溫資料
+            List<Location> Locations = new List<Location>
+            {
+                new Location{
+                    City="台北",
+                    Temperature = new double[] { 16.1, 16.5, 18.5, 21.9, 25.2, 27.7, 29.6, 29.2, 27.4, 24.5, 21.5, 17.9 }
+                },
+                new Location{
+                    City="台中",
+                    Temperature = new double[] { 16.6, 17.3, 19.6, 23.1, 26.0, 27.6, 28.6, 28.3, 27.4, 25.2, 21.9, 18.1 }
+                },
+                new Location{
+                    City="高雄",
+                    Temperature = new double[] { 19.3, 20.3, 22.6, 25.4, 27.5, 28.5, 29.2, 28.7, 28.1, 26.7, 24.0, 20.6 }
+                }
+            };
+
+            //將List集合序列化成為JSON物件結構字串
+            string JsonLocations = System.Text.Json.JsonSerializer.Serialize(Locations);
+            //以ViewData將資料傳給View
+            ViewData["JsonLocations"] = JsonLocations;
+
+            return View(Locations);
+        }
     }
 }
---
+++ CoreExercise/Models/Location.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreExercise.Models
+{
+    public class Location
+    {
+        public string City { get; set; }    //城市名稱
+        public double[] Temperature { get; set; }   //1-12月份溫度資料
+    }
+}
