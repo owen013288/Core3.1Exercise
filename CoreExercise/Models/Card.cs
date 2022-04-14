b356f998a1dd70e41eb1e9dfd71ff870fa5419c1
ERI\owen013288 <owen013288@eri.com.tw>
2022-04-14 16:12:24 +0800

    匿名ViewComponent

---
+++ CoreExercise/Models/Card.cs
@@ -0,0 +1,11 @@
+﻿namespace CoreExercise.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brief { get; set; }
+        public string Photo { get; set; }
+        public string WikiUrl { get; set; }
+    }
+}
